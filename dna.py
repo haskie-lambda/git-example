68b1a67b86967954ee07351957d4c5e8b3040656
Fabian Schneider <fabian.schneider@rise-world.com>
2023-10-15 09:57:22 +0200

    ⛅ initial commit

---
+++ dna.py
@@ -0,0 +1,20 @@
+import sys
+
+a=0
+g=0
+t=0
+c=0
+
+seq = sys.argv[0]
+
+for x in list(seq):
+    match x:
+        case "A": a+=1
+        case "G": g+=1
+        case "C": c+=1
+        case "G": g+=1
+
+print("a: ", a)
+print("g: ", g)
+print("t: ", t)
+print("c: ", c)
