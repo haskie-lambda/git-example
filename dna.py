802e2430f15454391db85aa108152799a30d7bdb
Fabian Schneider <fabian.schneider@rise-world.com>
2023-10-15 10:51:20 +0200

    adds complex args handling with version

--- dna.py
+++ dna.py
@@ -4,10 +4,18 @@ a=0
 g=0
 t=0
 c=0
+dnaArgs=[]
+
+match sys.argv:
+    case [_, "version"]:
+        print("version 1.0")
+        quit()
+    case seqs:
+        dnaArgs = seqs
 
 # some other comment
 # multiple args handling
-for seq in sys.argv:
+for seq in dnaArgs:
     for x in list(seq):
         match x:
             case "A": a+=1
