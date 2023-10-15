haskie-lambda/git-example

commit 470bc55c1c98486b9efeda459b5b4a88217f2abd

adds sth that doesn't work

dna.py
@@ -22,7 +22,7 @@ for seq in dnaArgs:
             case "G": g+=1
             case "C": c+=1
             case "G": g+=1
-
+xwxxxx
 print("A:", a)
 print("G:", g)
 print("T:", t)

commit ad7be0bf4af71ec049025bbf6ec714d041b06413

fixes some bug
Fixes bug introduced in 470bc55c1c98486b9efeda459b5b4a88217f2abd

dna.py
@@ -22,7 +22,7 @@ for seq in dnaArgs:
             case "G": g+=1
             case "C": c+=1
             case "G": g+=1
-xwxxxx
+
 print("A:", a)
 print("G:", g)
 print("T:", t)